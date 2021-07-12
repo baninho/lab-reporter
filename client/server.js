e258efba11e042ec1772341b78bd0796288e8fcb
baninho <florianimbt@gmail.com>
2021-07-12 10:07:25 -0400

    feature: use express to serve app from eb

---
+++ client/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log('listening on :' + port);
+});
+
