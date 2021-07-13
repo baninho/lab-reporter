e715087188dac4964e88600fadb0e66211d9c576
baninho <florianimbt@gmail.com>
2021-07-13 15:11:42 -0400

    feature: configure callback url for deployment

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,5 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'baninho.eu.auth0.com',            // Auth0 domain
   clientId: 'KQVJ4a6Bqzgs51ldFCWD427VGukqUSNj',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://lab-report-client-dev.eu-central-1.elasticbeanstalk.com/callback'
 }
