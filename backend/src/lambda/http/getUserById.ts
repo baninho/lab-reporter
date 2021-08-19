20aa7548533051d9018b3a3ff0d1c9d546823a56
baninho <florianimbt@gmail.com>
2021-08-19 17:55:02 -0500

    resolve: use headers instead of path parameter

--- backend/src/lambda/http/getUserById.ts
+++ backend/src/lambda/http/getUserById.ts
@@ -12,29 +12,33 @@ import { User } from '../../models/User'
 const logger = createLogger('getUserById')
 
 /**
- * Get user by ID
- */
+* Get user by ID
+*/
 export const handler = middy( async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const userId = getUserId(event)
-  const userIdRequest = event.pathParameters.userId
-
-  logger.info(`getting user ${userId}`)
-  try {
-    const user: User = await getUserById(userIdRequest)
-
-    // For now only return their own user info
-    // TODO: admin access, public access
-    const permission: boolean = userId === user.userId
-    const body: string = permission ? JSON.stringify(user) : 'No permission'
-    const statusCode = permission ? 200 : 403
-
+  const userIdRequest = event.headers.userid
+  
+  logger.info(`request user ${JSON.stringify(event.headers)} for ${userId}`)
+  
+  // For now only return their own user info
+  // TODO: admin access, public access
+  if (userId !== userIdRequest) {
     return {
-      statusCode,
-      body
+      statusCode: 403,
+      body: 'No permission'
     }
-  } catch (e) {
-    throw (e)
   }
+  
+  logger.info(`getting user ${userIdRequest}`)
+  
+  const user: User = await getUserById(userIdRequest)
+  logger.info(`got user ${JSON.stringify(user)}`)
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({user})
+  }
+  
 })
 
 handler.use(cors({
