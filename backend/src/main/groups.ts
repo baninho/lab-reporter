fd920166698faeb726b4059f9ea12a69e8abbe96
baninho <florianimbt@gmail.com>
2021-08-20 17:21:17 -0500

    refactor whitespace

--- backend/src/main/groups.ts
+++ backend/src/main/groups.ts
@@ -6,6 +6,7 @@ const groupAccess = new GroupAccess()
 export async function createGroup(group:Group) {
   return await groupAccess.createGroup(group)
 }
+
 export async function getGroups(): Promise<Group[]> {
   return await groupAccess.getGroups()
 }
