423818a565e35128cc4847190e3f7bed68b8b02c
baninho <florianimbt@gmail.com>
2021-08-20 11:27:26 -0500

    feature: increase padding to make room for menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -50,7 +50,7 @@ export default class App extends Component<AppProps, AppState> {
             }
             `}
           </style>
-          <Segment style={{ padding: '4em 0em' }} vertical>
+          <Segment style={{ padding: '6em 0em' }} vertical>
             <Grid container stackable verticalAlign="middle">
               <Grid.Row>
                 <Grid.Column width={16}>
