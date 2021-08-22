3809d22c25722613e37792ef8f9161c7e7180a53
baninho <florianimbt@gmail.com>
2021-08-22 08:58:43 -0500

    refactor: move entry list into separate component

--- client/src/components/Entries.tsx
+++ client/src/components/Entries.tsx
@@ -1,15 +1,10 @@
 import { History } from 'history'
 import * as React from 'react'
-import { Link } from 'react-router-dom'
 import {
-  Button,
   Divider,
   Grid,
-  Icon,
   Input,
-  Image,
-  Loader,
-  Container
+  Loader
 } from 'semantic-ui-react'
 
 import { createEntry, deleteEntry, getEntries } from '../api/entries-api'
@@ -17,6 +12,7 @@ import { getGroups } from '../api/groups-api'
 import Auth from '../auth/Auth'
 import { Entry } from '../types/Entry'
 import { Group } from '../types/Group'
+import { EntryList } from './EntryList'
 
 interface EntriesProps {
   auth: Auth
@@ -148,55 +144,12 @@ export class Entries extends React.PureComponent<EntriesProps, EntriesState> {
 
   renderEntriesList() {
     return (
-      <Container>
-        {this.state.entries.map((entry, pos) => {
-          const group = this.state.groups.find((g) => {return g.groupId === entry.groupId})
-          return (
-            <Grid>
-            <Grid.Row key={entry.entryId}>
-              <Grid.Column width={7} verticalAlign="middle">
-                <Link to={`/entries/${entry.entryId}`}><h3>{entry.name}</h3></Link>
-              </Grid.Column>
-              <Grid.Column width={4}>
-                {group ? group.name : ''}
-              </Grid.Column>
-              <Grid.Column width={3} floated="right">
-                {entry.createdAt}
-              </Grid.Column>
-              <Grid.Column width={2} floated="right">
-                <Button
-                  icon
-                  color="red"
-                  onClick={() => this.onEntryDelete(entry.entryId)}
-                  floated="right"
-                >
-                  <Icon name="delete" />
-                </Button>
-                <Button
-                  icon
-                  color="blue"
-                  onClick={() => this.onEditButtonClick(entry.entryId)}
-                  floated="right"
-                >
-                  <Icon name="pencil" />
-                </Button>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={2}>
-              {entry.attachments[0] && (
-                <Image src={entry.attachments[0].attachmentUrl} size="small" wrapped />
-              )}
-              </Grid.Column>
-              <Grid.Column  width={14}>
-                <span style={{whiteSpace: "pre-wrap"}}>{entry.body}</span>
-              </Grid.Column>
-            </Grid.Row>
-            <Divider />
-            </Grid>
-          )
-        })}
-      </Container>
+      <EntryList {...{
+        entries: this.state.entries,
+        groups: this.state.groups,
+        onEntryDelete: this.onEntryDelete,
+        onEditButtonClick: this.onEditButtonClick
+      }}/>
     )
   }
 }
