5eabf3565ccf67d72cd67ebe5c800d04dfdca755
baninho <florianimbt@gmail.com>
2021-08-23 11:01:52 -0500

    refactor: add comments

--- client/src/api/groups-api.ts
+++ client/src/api/groups-api.ts
@@ -3,6 +3,12 @@ import { apiEndpoint } from '../config'
 import { Group } from '../types/Group'
 import * as uuid from 'uuid'
 
+/**
+ * This will return all the existing groups to any user
+ * 
+ * @param idToken auth0 id token
+ * @returns all existing groups
+ */
 export async function getGroups(idToken: string): Promise<Group[]> {
   console.log(`Fetching groups`)
 
@@ -15,7 +21,16 @@ export async function getGroups(idToken: string): Promise<Group[]> {
   return response.data.items
 }
 
+/**
+ * create new Group in the database
+ * 
+ * @param idToken auth0 id token
+ * @param name new group name
+ * @returns newly created group
+ */
 export async function createGroup(idToken: string, name: string): Promise<Group> {
+  // TODO: id has to be assigned by backend to avoid replacing groups to gain access to entries
+  // should be implemented through a GroupRequest
   const group: Group = new Group(uuid.v4(), name)
   const response = await Axios.post(`${apiEndpoint}/groups`, JSON.stringify(group), {
     headers: {
